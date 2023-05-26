8cdf45a0b3d1bc90ccbe8440fd374e9efd015898
sefaca <sefaca24@gmail.com>
2023-05-26 11:55:42 +0200

    Carpeta scripts

---
+++ script1-falcon.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* @author Sergio Falcón De la Calle
+* @version 1.0.0
+*
+*/
+/**
+* Esta función devuelve la suma de los argumentos
+* enviados en la llamada
+* @param integer $num1 primer número a sumar
+* @param integer $num2 segundo número a sumar
+* @return integer resultado de la suma de dos números enteros
+*
+*/
+function suma ($num1, $num2)
+{
+return $num1 + $num2;
+}
+/**
+* Esta función devuelve la resta de los argumentos
+* enviados en la llamada
+* @param integer $num1 primer número a restar (minuendo)
+* @param integer $num2 segundo número a restar (sustraendo)
+* @return integer resultado de la resta de dos números enteros
+*
+*/
+function resta($num1, $num2,)
+{
+ return $num1 - $num2;
+}
+/**
+* Esta función devuelve un saludo con el nombre que le
+* enviamos en la llamada
+* @param string $nombre Es el nombre de la persona a la que saludamos
+* @return string Saludo a la persona solcitada
+*
+*/
+function saluda($nombre)
+{
+ echo "Hola ".$nombre;
+}
+echo "Ejemplo de funciones en PHP</br>";
+saluda("Sergio");
+echo "<br/> La diferencia entre 25 y 20 es : ".resta(25,20);
+echo "<br/> La suma de 25 y 20 es : ".suma(25,20);
+echo "<br/> Hasta pronto!!<br/>";
+?>
---
+++ scrpit2-falcon.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* @author Sergio Falcón De la Calle
+* @version 1.0.0
+*
+*/
+/**
+*Este script contiene una clase llamada Operaciones que tiene dos métodos: multiplicacion y division.
+*El método multiplicacion toma dos números como entrada y devuelve el resultado de multiplicarlos.
+*El método division también toma dos números como entrada y devuelve el cociente de la división del primer número entre el segundo.
+*/
+
+class Operaciones {
+
+    /**Clase que realiza operaciones matemáticas básicas.
+    * Esta función devuelve la multiplicación de los argumentos
+    * enviados en la llamada
+    * @param integer $a primer número a multiplicar 
+    * @param integer $b segundo número a multiplicar
+    * @return integer resultado de la multiplicación de dos números enteros
+    *
+    */
+    public function multiplicacion($a, $b){
+        return $a * $b;
+    }
+    
+    /**
+    * Esta función devuelve la división de los argumentos
+    * enviados en la llamada
+    * @param integer $a primer número a dividir 
+    * @param integer $b segundo número a dividir
+    * @return integer resultado de la división de dos números enteros
+    *
+    */
+    public function division($a, $b){
+        return $a / $b;
+    }
+
+}
+
+$operaciones = new Operaciones();
+
+echo "Ejemplo de funciones en PHP</br>";
+echo "<br/> La multiplicación entre 5 y 2 es : " . $operaciones->multiplicacion(5,2);
+echo "<br/> La división de 25 y 5 es : ". $operaciones->division(25,5);
+echo "<br/> Hasta pronto!!<br/>";
+?>
